6c4d347956878420da9d3d270590342aebfe193c
jacky <jacky123789456@gmail.com>
2022-09-26 17:52:26 +0800

    新增ShoppingCart.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import logo from './assets/images/logo.svg'
 import robots from './mookdata/robot.json'
 import Robot from './components/robot';
 import styles from './App.module.css'
+import ShoppingCart from './components/ShoppingCart'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <img src={logo} className={styles.appLogo} alt="" />
         <h1>羅伯特機器人online購物平台</h1>
       </div>
+      <ShoppingCart></ShoppingCart>
       <div className={ styles.robotList }>
         { robots.map((r, index) => {
           return <Robot id={r.id} name={r.name} email={r.email} />
--- src/custom.d.ts
+++ src/custom.d.ts
@@ -1,4 +1,5 @@
-declare module "*.css" { // 只要import以css為後綴的文件都會遵循以下的規定
+// 只要import以css為後綴的文件都會遵循以下的規定
+declare module "*.css" {
   const css: { [key: string]: string}
   export default css
 }
