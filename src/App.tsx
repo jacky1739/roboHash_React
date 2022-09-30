332457be7cf47e9d190580350e6b9c819909ec47
jacky <jacky123789456@gmail.com>
2022-09-30 15:58:57 +0800

    3-14 setState非同步處理

--- src/App.tsx
+++ src/App.tsx
@@ -11,19 +11,27 @@ interface Props {
 
 interface State {
   robotGallery: any[]
+  count: number
 }
 
 class App extends React.Component<Props, State> {
   constructor (props) {
     super(props)
     this.state = {
-      robotGallery: []
+      robotGallery: [],
+      count: 0
     }
   }
 
   componentDidMount(): void {
     fetch('https://jsonplaceholder.typicode.com/users')
-    .then(res => res.json())
+    // 在這裡回傳的是一個 Promise 函式 因此還需要再用一個.then來接收
+    .then( res => {
+      console.log(res)
+      return (
+        res.json()
+      )
+    })
     .then(data => this.setState({robotGallery: data}))
   }
 
@@ -34,6 +42,19 @@ class App extends React.Component<Props, State> {
           <img src={logo} className={styles.appLogo} alt="" />
           <h1>羅伯特機器人online購物平台</h1>
         </div>
+        <button onClick={() => {
+          this.setState((prevState, prevPops) => {
+            return { count: prevState.count + 1 }
+          }, () => {
+            console.log(this.state.count)
+          })
+          this.setState((prevState, prevPops) => {
+            return { count: prevState.count + 1 }
+          }, () => {
+            console.log(this.state.count)
+          })
+        }}>+ 1</button>
+        <span>state : {this.state.count}</span>
         <ShoppingCart></ShoppingCart>
         <div className={ styles.robotList }>
           { this.state.robotGallery.map((r, index) => {
