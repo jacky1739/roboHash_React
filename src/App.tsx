5b629dcb85402f8e7d2ac26c6369722d94d09a42
jacky <jacky123789456@gmail.com>
2022-10-03 17:21:45 +0800

    新增useState 改成function component

--- src/App.tsx
+++ src/App.tsx
@@ -1,69 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './assets/images/logo.svg'
 // import robots from './mookdata/robot.json'
-import Robot from './components/robot';
+// import Robot from './components/robot';
 import styles from './App.module.css'
 import ShoppingCart from './components/ShoppingCart'
 
-interface Props {
+const App: React.FC = () => { 
+  const [count, setCount] = useState(0)
 
-}
-
-interface State {
-  robotGallery: any[]
-  count: number
-}
-
-class App extends React.Component<Props, State> {
-  constructor (props) {
-    super(props)
-    this.state = {
-      robotGallery: [],
-      count: 0
-    }
-  }
-
-  componentDidMount(): void {
-    fetch('https://jsonplaceholder.typicode.com/users')
-    // 在這裡回傳的是一個 Promise 函式 因此還需要再用一個.then來接收
-    .then( res => {
-      console.log(res)
-      return (
-        res.json()
-      )
-    })
-    .then(data => this.setState({robotGallery: data}))
-  }
-
-  render () {
-    return (
-      <div className={ styles.app }>
-        <div className={styles.appHeader}>
-          <img src={logo} className={styles.appLogo} alt="" />
-          <h1>羅伯特機器人online購物平台</h1>
-        </div>
-        <button onClick={() => {
-          this.setState((prevState, prevPops) => {
-            return { count: prevState.count + 1 }
-          }, () => {
-            console.log(this.state.count)
-          })
-          this.setState((prevState, prevPops) => {
-            return { count: prevState.count + 1 }
-          }, () => {
-            console.log(this.state.count)
-          })
-        }}>+ 1</button>
-        <span>state : {this.state.count}</span>
-        <ShoppingCart></ShoppingCart>
-        <div className={ styles.robotList }>
-          { this.state.robotGallery.map((r, index) => {
-            return <Robot id={r.id} name={r.name} email={r.email} />
-          }) }
-        </div>
+  return (
+    <div className={ styles.app }> 
+      <div className={styles.appHeader}>
+        <img src={logo} className={styles.appLogo} alt="" />
+        <h1>羅伯特機器人online購物平台</h1>
       </div>
-    )
-  }
+      <button onClick={() => {
+        setCount(count + 1)
+      }}>+ 1</button>
+      <span>state : {count}</span>
+      <ShoppingCart></ShoppingCart>
+      {/* <div className={ styles.robotList }>
+        { this.state.robotGallery.map((r, index) => {
+          return <Robot id={r.id} name={r.name} email={r.email} />
+        }) }
+      </div> */}
+    </div>
+  )
 }
 
 export default App;
