3b1bb29b807634df2b403205dd027f32b8d6cc94
jacky <jacky123789456@gmail.com>
2022-10-04 16:59:32 +0800

    使用useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './assets/images/logo.svg'
 // import robots from './mookdata/robot.json'
-// import Robot from './components/robot';
+import Robot from './components/robot';
 import styles from './App.module.css'
 import ShoppingCart from './components/ShoppingCart'
 
-const App: React.FC = () => { 
+const App: React.FC = () => {
   const [count, setCount] = useState(0)
+  const [robotGallery, setRobotGallery] =useState<any>([])
+
+  useEffect(() => {
+    document.title = `點擊${count}`
+  }, [count])
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(data => setRobotGallery(data))
+  }, [])
 
   return (
-    <div className={ styles.app }> 
+    <div className={ styles.app }>
       <div className={styles.appHeader}>
         <img src={logo} className={styles.appLogo} alt="" />
         <h1>羅伯特機器人online購物平台</h1>
@@ -19,11 +30,11 @@ const App: React.FC = () => {
       }}>+ 1</button>
       <span>state : {count}</span>
       <ShoppingCart></ShoppingCart>
-      {/* <div className={ styles.robotList }>
-        { this.state.robotGallery.map((r, index) => {
+      <div className={ styles.robotList }>
+        { robotGallery.map((r) => {
           return <Robot id={r.id} name={r.name} email={r.email} />
         }) }
-      </div> */}
+      </div>
     </div>
   )
 }
