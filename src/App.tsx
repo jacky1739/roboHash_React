a9b9917fa7eea8c225638d7fbc603394700f51f4
jacky <jacky123789456@gmail.com>
2022-10-04 17:18:41 +0800

    在useEffect裡面使用async await

--- src/App.tsx
+++ src/App.tsx
@@ -8,15 +8,23 @@ import ShoppingCart from './components/ShoppingCart'
 const App: React.FC = () => {
   const [count, setCount] = useState(0)
   const [robotGallery, setRobotGallery] =useState<any>([])
+  const [loading, setLoading] = useState<boolean>(false)
 
   useEffect(() => {
     document.title = `點擊${count}`
   }, [count])
 
+  // useEffect 返回的是一個函數或null, 
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-    .then(data => setRobotGallery(data))
+    const fetchData = async () => {
+      setLoading(true)
+      const response = await fetch('https://jsonplaceholder.typicode.com/users')
+      const data = await response.json()
+      setRobotGallery(data)
+      setLoading(false)
+    }
+
+    fetchData()
   }, [])
 
   return (
@@ -30,11 +38,15 @@ const App: React.FC = () => {
       }}>+ 1</button>
       <span>state : {count}</span>
       <ShoppingCart></ShoppingCart>
-      <div className={ styles.robotList }>
+      { !loading ? (
+        <div className={ styles.robotList }>
         { robotGallery.map((r) => {
           return <Robot id={r.id} name={r.name} email={r.email} />
         }) }
       </div>
+      ) : (
+        <h2>loading 加載中</h2>
+      )}
     </div>
   )
 }
